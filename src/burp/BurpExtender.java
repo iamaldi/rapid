ced444934f768cc04e3d3936442749093ab7befc
iamaldi <dialcode@outlook.com>
2019-09-28 23:52:13 +0300

    Implement context awareness
    Closes #3

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,73 +1,70 @@
 package burp;
 
+import rapid.Rapid;
+
+import javax.swing.*;
 import java.awt.event.InputEvent;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JMenuItem;
-
-import rapid.Rapid;
-
 public class BurpExtender implements IBurpExtender, IContextMenuFactory, IContextMenuInvocation {
 
-	private IBurpExtenderCallbacks callbacks;
-	private Rapid rapid;
+    private IBurpExtenderCallbacks callbacks;
 
-	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
-		this.callbacks = callbacks;
-		this.callbacks.setExtensionName("Rapid");
-		this.callbacks.registerContextMenuFactory(this);
-	}
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        this.callbacks = callbacks;
+        this.callbacks.setExtensionName("Rapid");
+        this.callbacks.registerContextMenuFactory(this);
+    }
 
-	@Override
-	public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
+    @Override
+    public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
+        byte context = invocation.getInvocationContext();
+        List<JMenuItem> menu = new ArrayList<JMenuItem>();
+        if (context == IContextMenuInvocation.CONTEXT_INTRUDER_ATTACK_RESULTS || context == IContextMenuInvocation.CONTEXT_INTRUDER_PAYLOAD_POSITIONS) {
+            return menu;
+        } else {
+            Rapid rapid = new Rapid(callbacks, invocation);
 
-		// Get a Rapid instance
-		rapid = new Rapid(callbacks, invocation);
+            menu.add(rapid.getMenuEntry("Rapid - Save HTTP Request and Response", false));
+            menu.add(rapid.getMenuEntry("Rapid - Save Files & Screenshot", true));
+            return menu;
+        }
+    }
 
-		// Create menu
-		List<JMenuItem> menu = new ArrayList<JMenuItem> ();
+    @Override
+    public int getToolFlag() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-		// Add menu entries
-		menu.add(rapid.getMenuEntry("Rapid - Save HTTP Request and Response", false));
-		menu.add(rapid.getMenuEntry("Rapid - Save Files & Screenshot", true));
+    @Override
+    public byte getInvocationContext() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-		return menu;
-	}
+    @Override
+    public int[] getSelectionBounds() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int getToolFlag() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public IHttpRequestResponse[] getSelectedMessages() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public byte getInvocationContext() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public InputEvent getInputEvent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int[] getSelectionBounds() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IHttpRequestResponse[] getSelectedMessages() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public InputEvent getInputEvent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IScanIssue[] getSelectedIssues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public IScanIssue[] getSelectedIssues() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
--- src/rapid/Rapid.java
+++ src/rapid/Rapid.java
@@ -1,9 +1,14 @@
 package rapid;
 
-import java.awt.AWTException;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
+import burp.IBurpExtenderCallbacks;
+import burp.IContextMenuInvocation;
+import burp.IHttpRequestResponse;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -14,200 +19,131 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
-import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.swing.filechooser.FileSystemView;
-
-import burp.IBurpExtenderCallbacks;
-import burp.IContextMenuInvocation;
-import burp.IHttpRequestResponse;;
-
 public class Rapid {
+    private IBurpExtenderCallbacks callbacks;
+    private IContextMenuInvocation invocation;
+    private String httpRequestFilenameSuffix = "-[HTTP-Request].txt";
+    private String httpResponseFilenameSuffix = "-[HTTP-Response].txt";
 
-	private IBurpExtenderCallbacks callbacks;
-	private IContextMenuInvocation invocation;
+    public Rapid(IBurpExtenderCallbacks callbacks, IContextMenuInvocation invocation) {
+        this.callbacks = callbacks;
+        this.invocation = invocation;
+    }
 
-	private String httpRequestFilenameSuffix = "-[HTTP-Request].txt";
-	private String httpResponseFilenameSuffix = "-[HTTP-Response].txt";
+    public JMenuItem getMenuEntry(String entryName, boolean captureScreenshot) {
+        JMenuItem menuEntry = new JMenuItem(entryName);
+        menuEntry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    JFrame fileExplorerFrame = new JFrame();
+                    JFileChooser fileExplorer = createFileExplorer(fileExplorerFrame, captureScreenshot);
+                    fileExplorer.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    fileExplorer.setFileFilter(new FileNameExtensionFilter("Text Files Only", "txt"));
+                    fileExplorer.setDialogTitle("Choose a file to save to");
+                    fileExplorerFrame.add(fileExplorer);
+                    fileExplorer.showSaveDialog(fileExplorerFrame);
+                } catch (Exception e1) {
+                    throw new RuntimeException(e1);
+                }
+            }
+        });
+        return menuEntry;
+    }
 
-	public Rapid(IBurpExtenderCallbacks callbacks, IContextMenuInvocation invocation) {
-		this.callbacks = callbacks;
-		this.invocation = invocation;
-	}
+    private JFileChooser createFileExplorer(JFrame fileExplorerFrame, boolean captureScreenshot) {
 
-	public JMenuItem getMenuEntry(String entryName, boolean captureScreenshot) {
-		// Create menu entry
-		JMenuItem menuEntry = new JMenuItem(entryName);
+        return new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory()) {
+            private static final long serialVersionUID = 1L;
 
-		// Listen for clicks on the menu entry
-		menuEntry.addActionListener(new ActionListener() {
+            @Override
+            public void approveSelection() {
+                File destinationFile = getSelectedFile();
+                if (destinationFile.exists()) {
+                    int confirmOverwrite = JOptionPane.showConfirmDialog(null, "This file already exists. Are you sure?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				try {
-					// Create frame
-					JFrame fileExplorerFrame = new JFrame();
+                    // Overwrite existing file
+                    if (confirmOverwrite == JOptionPane.YES_OPTION) {
+                        saveDataToFile(destinationFile, confirmOverwrite);
+                        fileExplorerFrame.dispose();
+                        if (captureScreenshot) {
+                            try {
+                                captureScreenshot(destinationFile);
+                            } catch (AWTException | InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                } else {
+                    // Write data to a new file
+                    saveDataToFile(destinationFile, 1);
+                    fileExplorerFrame.dispose();
 
-					// Create file explorer
-					JFileChooser fileExplorer = createFileExplorer(fileExplorerFrame, captureScreenshot);
+                    // Check if we should also capture a screenshot
+                    if (captureScreenshot) {
+                        try {
+                            captureScreenshot(destinationFile);
+                        } catch (AWTException | InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            }
+        };
+    }
 
-					// Show files only
-					fileExplorer.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    private void saveDataToFile(File destinationFile, int confirmOverwrite) {
+        // Get HTTP Request / Response data
+        IHttpRequestResponse[] httpRequestResponse = invocation.getSelectedMessages();
+        ArrayList<String> filenames = new ArrayList<>(2);
 
-					// Restrict to only text files
-					fileExplorer.setFileFilter(new FileNameExtensionFilter("Text Files Only", "txt"));
+        if (confirmOverwrite == 0) {
+            if (destinationFile.getAbsoluteFile().getName().contains(httpRequestFilenameSuffix)) {
+                setFilenames(destinationFile.getPath(), filenames, 19);
+            } else if (destinationFile.getAbsoluteFile().getName().contains(httpResponseFilenameSuffix)) {
+                setFilenames(destinationFile.getPath(), filenames, 20);
+            } else if (destinationFile.getAbsoluteFile().getName().contains(".txt")) {
+                setFilenames(destinationFile.getPath(), filenames, 4);
+            }
 
-					// Set window title
-					fileExplorer.setDialogTitle("Choose a file to save to");
+        } else {
+            filenames.add(destinationFile.getPath().concat(httpRequestFilenameSuffix));
+            filenames.add(destinationFile.getPath().concat(httpResponseFilenameSuffix));
+        }
 
-					// Add fileExplorer to frame
-					fileExplorerFrame.add(fileExplorer);
+        // Write files on disk
+        for (int i = 0; i < 2; i++) {
+            File outfile = new File(filenames.get(i));
+            try {
+                PrintWriter outfileWriter = new PrintWriter(outfile);
+                if (i == 0) {
+                    outfileWriter.write(callbacks.getHelpers().bytesToString(httpRequestResponse[0].getRequest()));
+                    outfileWriter.close();
+                } else {
+                    outfileWriter.write(callbacks.getHelpers().bytesToString(httpRequestResponse[0].getResponse()));
+                    outfileWriter.close();
+                }
+            } catch (FileNotFoundException e2) {
+                throw new RuntimeException(e2);
+            }
+        }
+    }
 
-					// Show dialog
-					fileExplorer.showSaveDialog(fileExplorerFrame);
+    private void setFilenames(String filePath, ArrayList<String> filenames, int index) {
+        final String substring = filePath.substring(0, filePath.length() - index);
+        filenames.add(substring.concat(httpRequestFilenameSuffix));
+        filenames.add(substring.concat(httpResponseFilenameSuffix));
+    }
 
-				} catch (Exception e1) {
-					// Couldn't launch 
-					throw new RuntimeException(e1);
-				}
-			}
-
-		});
-
-		return menuEntry;
-	}
-
-	private JFileChooser createFileExplorer(JFrame fileExplorerFrame, boolean captureScreenshot) {
-
-		JFileChooser fileExplorer = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory()) {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public void approveSelection() {
-				File destinationFile = getSelectedFile();
-				if (destinationFile.exists()) {
-					int confirmOverwrite = JOptionPane.showConfirmDialog(null, "This file already exists. Are you sure?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-
-					// Overwrite existing file
-					if (confirmOverwrite == JOptionPane.YES_OPTION) {
-						writeOutputFiles(destinationFile, confirmOverwrite);
-						// Close fileExplorer
-						fileExplorerFrame.dispose();
-						if (captureScreenshot) {
-							try {
-								// Capture screenshot
-								captureScreenshot(destinationFile);
-							} catch (AWTException e) {
-								throw new RuntimeException(e);
-							} catch (InterruptedException e) {
-								throw new RuntimeException(e);
-							}
-						}
-					}
-				} else {
-					// Write to new file
-					writeOutputFiles(destinationFile, 1);
-
-					// Close fileExplorer
-					fileExplorerFrame.dispose();
-
-					// Check if we have to capture a screenshot
-					if (captureScreenshot) {
-						try {
-							// Capture screenshot
-							captureScreenshot(destinationFile);
-						} catch (AWTException e) {
-							throw new RuntimeException(e);
-						} catch (InterruptedException e) {
-							throw new RuntimeException(e);
-						}
-					}
-				}
-			}
-		};
-
-		return fileExplorer;
-	}
-
-	private void writeOutputFiles(File destinationFile, int confirmOverwrite) {
-		// Get HTTP Request / Response data
-		IHttpRequestResponse[] httpRequestResponse = invocation.getSelectedMessages();
-
-		// HTTP Request and Response filenames
-		ArrayList<String> filenames = new ArrayList<>(2);
-
-		// Overwriting existing files
-		if (confirmOverwrite == 0) {
-			// Check filename
-			if (destinationFile.getAbsoluteFile().getName().contains(httpRequestFilenameSuffix)) {
-				// HTTP Request file
-				setFilenames(destinationFile.getPath(), filenames, 19);
-			} else if (destinationFile.getAbsoluteFile().getName().contains(httpResponseFilenameSuffix)) {
-				// HTTP Response file
-				setFilenames(destinationFile.getPath(), filenames, 20);
-			} else if (destinationFile.getAbsoluteFile().getName().contains(".txt")) {
-				// Plain .txt file
-				setFilenames(destinationFile.getPath(), filenames, 4);
-			}
-
-			// Writing to new files
-		} else {
-			filenames.add(destinationFile.getPath().concat(httpRequestFilenameSuffix));
-			filenames.add(destinationFile.getPath().concat(httpResponseFilenameSuffix));
-		}
-
-		// Write files to disk
-		for (int i = 0; i<2; i++) {
-			// Create file
-			File outfile = new File(filenames.get(i));
-			try {
-				PrintWriter outfileWriter = new PrintWriter(outfile);
-				if (i == 0) {
-					// Write HTTP Request to file
-					outfileWriter.write(callbacks.getHelpers().bytesToString(httpRequestResponse[0].getRequest()));
-					outfileWriter.close();
-				} else {
-					// Write HTTP Response to file
-					outfileWriter.write(callbacks.getHelpers().bytesToString(httpRequestResponse[0].getResponse()));
-					outfileWriter.close();
-				}
-
-			} catch (FileNotFoundException e2) {
-				// File not found
-				throw new RuntimeException(e2);
-			}
-		}
-	}
-
-	// Rename files based on which one was selected. HTTP Request? HTTP Response? Simple .txt?
-	private void setFilenames(String filePath, ArrayList<String> filenames, int index) {
-		// Trim & alter filename suffix
-		// index indicates the level of trim based on which file was selected for overwriting
-		filenames.add(filePath.substring(0, filePath.length() - index).concat(httpRequestFilenameSuffix));
-		filenames.add(filePath.substring(0, filePath.length() - index).concat(httpResponseFilenameSuffix));
-	}
-
-	// Captures screenshot and saves it to file (PNG)
-	private void captureScreenshot(File destinationFile) throws AWTException,
-		InterruptedException {
-			// Delay the capture of the screenshot
-			try {
-				// Delay 1s for fileExplorerFrame to dispose
-				TimeUnit.SECONDS.sleep(1);
-
-				// Get screen
-				Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-
-				// Capture screen
-				BufferedImage capture = new Robot().createScreenCapture(screen);
-
-				// Save capture to file
-				ImageIO.write(capture, "PNG", new File(destinationFile.getPath().concat("-[IMG].png")));
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
+    private void captureScreenshot(File destinationFile) throws AWTException,
+            InterruptedException {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+            Rectangle screen = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage capture = new Robot().createScreenCapture(screen);
+            ImageIO.write(capture, "PNG", new File(destinationFile.getPath().concat("-[IMG].png")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
